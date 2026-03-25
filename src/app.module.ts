e6d347ee4007469db9ebc905db8d0ce96dfcad3d
Jony <jony378892@gmail.com>
2026-03-25 22:54:33 +0600

    Added cats route and added middleware for cats route

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController, CatsController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
